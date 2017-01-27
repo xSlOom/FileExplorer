6540bc9495d5fba1556fd06314ac4366201af3b6
CedricPEPS <cedric.p@codeur.online>
2017-01-27 15:48:18 +0100

    modif
    rajout des switch et entete

--- index.php
+++ index.php
@@ -14,7 +14,9 @@ $prev   = str_replace(realpath(dirname($path) . '/..'), '', realpath(dirname($pa
     </head>
     <body>
         <div class="container">
-            <p>File explorer made by Clément and Cédric.</p>
+            <div class="entete">
+            <h2 id="title">La spéléo du fichier made by Clément and Cédric.</h2>
+        </div>
             <a href="?path=<?php echo $prev; ?>"><img src="images/more.png"></a> <br />
             <?php
                 // chemin actuel : getcwd();
@@ -33,32 +35,97 @@ $prev   = str_replace(realpath(dirname($path) . '/..'), '', realpath(dirname($pa
                             if (is_file($path.$scan[$i])):
                                 switch (@strtolower($ext["extension"])):
                                     case "html":
-                                        $image = "images/html.png";
+                                        $image  = "images/html.png";
                                         break;
                                     case "php":
-                                        $image = "images/php.png";
+                                        $image  = "images/php.png";
                                         break;
                                     case "txt":
-                                        $image = "images/txt.png";
+                                        $image  = "images/txt.png";
                                         break;
                                     case "exe":
-                                        $image = "images/exe.png";
+                                        $image  = "images/exe.png";
                                         break;
                                     case "zip":
-                                        $image = "images/zip.png";
+                                        $image  = "images/zip.png";
                                         break;
                                     case "css":
-                                        $image = "images/css.png";
+                                        $image  = "images/css.png";
                                         break;
                                     case "js":
-                                        $image = "images/javascript.png";
+                                        $image  = "images/javascript.png";
+                                        break;
+                                    case "avi":
+                                        $image  = "images/avi.png";
+                                        break;
+                                    case "csv":
+                                        $image  = "images/csv.png";
+                                        break;
+                                    case "dbf":
+                                        $image  = "images/dbf.png";
+                                        break;
+                                    case "ai":
+                                        $image  = "images/ai.png";
+                                        break;
+                                    case "doc":
+                                    case "docx":
+                                        $image  = "images/doc.png";
+                                        break;
+                                    case "dwg":
+                                        $image  = "images/dwg.png";
+                                        break;
+                                    case "fla":
+                                        $image  = "images/fla.png";
+                                        break;
+                                    case "jpg":
+                                    case "jpeg":
+                                        $image  = "images/jpg.png";
+                                        break;
+                                    case "iso":
+                                        $image  = "images/iso.png";
+                                        break;
+                                    case "json":
+                                        $image  = "images/json-file.png";
+                                        break;
+                                    case "mp3":
+                                    case "mpeg3":
+                                        $image  = "images/mp3.png";
+                                        break;
+                                    case "mp4":
+                                    case "mpeg4":
+                                        $image  = "images/mp4.png";
+                                        break;
+                                    case "pdf":
+                                        $image  = "images/pdf.png";
                                         break;
                                     case "png":
-                                    case "jpg":
-                                        $image = "images/png.png";
+                                        $image  = "images/png.png";
+                                        break;
+                                    case "ppt":
+                                    case "pptx":
+                                        $image  = "images/ppt.png";
+                                        break;
+                                    case "psd":
+                                        $image  = "images/psd.png";
+                                        break;
+                                    case "rtf":
+                                        $image  = "images/rtf.png";
+                                        break;
+                                    case "svg":
+                                        $image  = "images/svg.png";
+                                        break;
+                                     case "xls":
+                                     case "xlsx":
+                                        $image  = "images/xls.png";
+                                        break;
+                                    case "xml":
+                                        $image  = "images/xml.png";
+                                        break;
+                                    case "rar":
+                                        $image  = "images/zip-1.png";
                                         break;
                                     default:
-                                        $image = "images/file.png";
+                                        $image  = "images/file.png";
                                         break;
                                 endswitch;
                             else:

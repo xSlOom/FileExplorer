b84aded01b45ca2d26f59c852c4a5a95fbcbf206
Clément <illuclem@gmail.com>
2017-01-27 11:14:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,17 @@
         <title>File explorer</title>
     </head>
     <body>
-        <p>File explorer made by Clément and Cédric.</p>
+        <div>
+            <p>File explorer made by Clément and Cédric.</p>
+        </div>
         <?php
-            $scan   = scandir("../");
+            $path   = "../";
+            $scan   = scandir($path);
             for ($i = 0; $i < sizeof($scan); $i++) {
                 if (!in_array($scan[$i], ["..", "."])) {
-                    $ext = pathinfo($scan[$i]);
-                    print $scan[$i] . " (" . (!empty($ext["extension"]) ? "C'est un fichier" : "C'est un dossier") . ") <br />";
+                    $ext    = pathinfo($scan[$i]);
+                    $inf    = !empty($ext["extension"]) ? "C'est un fichier" : "C'est un dossier";
+                    print '<a href="' . $path . $scan[$i] . '">' . $scan[$i] . ' (' . $inf . ') </a><br />';
                 }
             }
         ?>
